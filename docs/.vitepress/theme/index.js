94936b34af53bb69313745716b93955eefb7214d
Esther <w41621@yahoo.com.tw>
2026-09-13 20:00:48 +0800

    refactor: remove legacy global stylesheet imports

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,8 +2,6 @@ import DefaultTheme from 'vitepress/theme';
 import './styles/tokens.css';
 import './styles/layout.css';
 import './styles/shared.css';
-import './custom.css';
-import './relations.css';
 
 export default {
   extends: DefaultTheme
